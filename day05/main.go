31fd8833434a7f79407db366240f29b865678c2c
Alexis-Benamar <alexis.benamar@viacesi.fr>
2023-12-07 16:28:00 +0100

    update day05 add getMapped func

--- day05/main.go
+++ day05/main.go
@@ -7,6 +7,12 @@ import (
 	"time"
 )
 
+type Mapping struct {
+	Destination int
+	Source int
+	Length int
+}
+
 //go:embed example.txt
 var data string
 var lines []string
@@ -20,11 +26,63 @@ func init() {
 	lines = strings.Split(data, "\n")
 }
 
+func getMapped(nbToCheck int, mapToCompare [][]int) int {
+	/*
+		mapping = find mapping in mapToCompare where source < nbToCheck < (source + length)
+		if (mapping not found)
+			return nbToCheck
+		else
+			if (destination < source)
+				return seedNb - (source - destination)
+			else
+				return seedNb + (destination - source)
+	*/
+	var mapping []int
+
+	for _, potentialMap := range mapToCompare {
+		if nbToCheck >= potentialMap[1] && nbToCheck <= potentialMap[1] + potentialMap[2] {
+			mapping = potentialMap
+			break
+		}
+	}
+
+	fmt.Println(nbToCheck, mapping)
+
+	if len(mapping) == 0 {
+		return nbToCheck
+	}
+
+	if mapping[0] < mapping[1] {
+		return nbToCheck - (mapping[1] - mapping[0])
+	} else {
+		return nbToCheck + (mapping[0] - mapping[1])
+	}
+}
+
+func getSeedLocation(seed int) int {
+	/*
+		soil := getMapped(seed, seedToSoil)
+		fertilizer := getMapped(soil, soilToFertilizer)
+		water := getMapped(fertilizer, fertilizerToWater)
+		light := getMapped(water, waterToLight)
+		temp := getMapped(light, lightToTemp)
+		humidity := getMapped(temp, tempToHumidity)
+		location := getMapped(humidity, humidityToLocation)
+
+		return location
+	*/
+
+	return 0
+}
+
 func main() {
 	start := time.Now()
 
 	// Code here
+	res := getMapped(6, [][]int{{0, 5, 4}, {6, 10, 4}, {52, 50, 48}})
+
+	fmt.Println("returned", res)
 
 	elapsed := time.Since(start)
-	fmt.Println("\nExecution time", elapsed.Seconds(), "s")
+	fmt.Printf("\nExecution time %f s\n", elapsed.Seconds())
 }
